04ffa53172c533490723f5b7c41d31180bcebe0d
Arien Shibani <aaari94@gmail.com>
2023-06-23 10:49:44 +0200

    feat: :rocket: Init project

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# CCommits-scope-builder
-Simple node script that populates the array of conventional commit scopes for you with every folder and file in your current repository.
+# VSCode CComits Scope Builder
+
+Node script that automatically populate CComits scopes in your `.vscode/settings.json` file used by the [CComits VSCode extension](https://marketplace.visualstudio.com/items?itemName=vivaxy.vscode-conventional-commits) to help you write better commit messages in large projects and mono-repos.
+
+## Conventional Commits
+
+[Conventional Commits specification](https://www.conventionalcommits.org/en/v1.0.0/) is a convention for writing commit messages. It provides a set of rules for creating an explicit commit history.
+
+## How does it work?
+
+The script simply traverses your working directory, stores the names of all folders and sub-folders in a list before iterating over that list and creating an entry for each element in your `.vscode/settings.json`
+
+### Example Project Structure
+
+```bash
+- .vscode
+- front-end
+    - components
+    - public
+- back-end
+- random
+
+```
+
+Given the folder structure above, after placing the script at root level and executing it with `node index.js` you get the results depicted below. An automatically populated list of scopes for your project.
+
+### Result
+
+```json
+{
+  // .vscode/settings.json
+  "conventionalCommits.scopes": [
+    "backend",
+    "front-end",
+    "front-end/components",
+    "front-end/public",
+    "random",
+  ]
+}
+```
+
+### Why folders as scopes?
+
+Scopes should indicate the module, package, or area of the code that was affected by the changes made in the commit. Rather than using specific file paths, directories *feels*   better to use.
---
+++ index.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+const path = require('path');
+// Get the current working directory
+const rootDir = process.cwd();
+
+
+// Helper function used to prevent duplicates in the file paths that sometimes occur.
+const removeDuplicateSegments = (str) => {
+    const segments = str.split('/');
+    const uniqueSegments = [];
+
+    segments.forEach((segment) => {
+        if (segment && !uniqueSegments.includes(segment)) {
+            uniqueSegments.push(segment);
+        }
+    });
+
+    return uniqueSegments.join('/');
+}
+
+// Helper file, used to make sure that the "settings.json" file exists, and that the ".vscode" folder exists.
+const ensureVsCodeFolderAndSettingsFile = () => {
+    const vscodeDir = path.join(process.cwd(), '.vscode');
+    const settingsPath = path.join(vscodeDir, 'settings.json');
+
+    if (!fs.existsSync(vscodeDir)) {
+    fs.mkdirSync(vscodeDir);
+    }
+
+    if (!fs.existsSync(settingsPath)) {
+    fs.writeFileSync(settingsPath, JSON.stringify({}, null, 2));
+    }
+}
+
+function traverseDirectory(dir, ignoreList = []) {
+    ensureVsCodeFolderAndSettingsFile()
+    const files = fs.readdirSync(dir);
+    let results = [];
+    files.forEach(file => {
+      const fullPath = path.join(dir, file);
+      if (ignoreList.includes(file)) {
+        return;
+      }
+      if (fs.statSync(fullPath).isDirectory()) {
+        const relativePath = path.relative(rootDir, fullPath);
+        results.push(removeDuplicateSegments(path.join(relativePath)));
+        results = results.concat(traverseDirectory(fullPath, ignoreList));
+      }
+    });
+    return results;
+  }
+
+// Define a list of folders to ignore
+const ignoreList = ['.git', '.github', 'node_modules'];
+
+// Get the path to the settings.json file
+const settingsPath = path.join(rootDir, '.vscode', 'settings.json');
+
+// Read the current contents of the settings.json file
+const settingsContent = fs.readFileSync(settingsPath, 'utf8');
+
+// Parse the JSON content
+const settingsObject = JSON.parse(settingsContent);
+
+// Add the new key to the settings object
+settingsObject["conventionalCommits.scopes"] = traverseDirectory(rootDir, ignoreList);
+settingsObject["conventionalCommits.emojiFormat"] = "emoji"
+
+try {
+    // Write the updated settings object to the settings.json file
+    fs.writeFileSync(settingsPath, JSON.stringify(settingsObject, null, 2));
+} catch (error) {
+    console.log("Something failed..", error)
+}
