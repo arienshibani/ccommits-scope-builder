bccfef2d94c1dfe3d423e3780de366a21ee2bb76
Arien Shibani <aaari94@gmail.com>
2023-06-23 12:28:27 +0200

    refactor(index.js): ♻️ Lint index.js

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
+// Get the current working directory
 const fs = require('fs');
 const path = require('path');
-// Get the current working directory
-const rootDir = process.cwd();
 
-
-// Helper function used to prevent duplicates in the file paths that sometimes occur.
+// Used to prevent duplicates in the file paths that sometimes occur.
 const removeDuplicateSegments = (str) => {
-    const segments = str.split('/');
-    const uniqueSegments = [];
+  const segments = str.split('/');
+  const uniqueSegments = [];
 
-    segments.forEach((segment) => {
-        if (segment && !uniqueSegments.includes(segment)) {
-            uniqueSegments.push(segment);
-        }
-    });
+  segments.forEach((segment) => {
+    if (segment && !uniqueSegments.includes(segment)) {
+      uniqueSegments.push(segment);
+    }
+  });
 
-    return uniqueSegments.join('/');
-}
+  return uniqueSegments.join('/');
+};
 
-// Helper file, used to make sure that the "settings.json" file exists, and that the ".vscode" folder exists.
+// Makes sure that the "settings.json" file exists, and that the ".vscode" folder exists.
 const ensureVsCodeFolderAndSettingsFile = () => {
-    const vscodeDir = path.join(process.cwd(), '.vscode');
-    const settingsPath = path.join(vscodeDir, 'settings.json');
+  const vscodeDir = path.join(process.cwd(), '.vscode');
+  const settingsPath = path.join(vscodeDir, 'settings.json');
 
-    if (!fs.existsSync(vscodeDir)) {
+  if (!fs.existsSync(vscodeDir)) {
     fs.mkdirSync(vscodeDir);
-    }
-
-    if (!fs.existsSync(settingsPath)) {
-    fs.writeFileSync(settingsPath, JSON.stringify({}, null, 2));
-    }
-}
-
-const traverseDirectory = (dir, ignoreList = []) => {
-    const files = fs.readdirSync(dir);
-    let results = [];
-    files.forEach(file => {
-      const fullPath = path.join(dir, file);
-      if (ignoreList.includes(file)) {
-        return;
-      }
-      if (fs.statSync(fullPath).isDirectory()) {
-        const relativePath = path.relative(rootDir, fullPath);
-        results.push(removeDuplicateSegments(path.join(relativePath)));
-        results = results.concat(traverseDirectory(fullPath, ignoreList));
-      }
-    });
-    return results;
   }
 
+  if (!fs.existsSync(settingsPath)) {
+    fs.writeFileSync(settingsPath, JSON.stringify({}, null, 2));
+  }
+};
+
+// Main function that traverses the directory and returns a list of all the folders.
+const rootDir = process.cwd();
+const traverseDirectory = (dir, ignoreList = []) => {
+  const files = fs.readdirSync(dir);
+  let results = [];
+  files.forEach((file) => {
+    const fullPath = path.join(dir, file);
+    if (ignoreList.includes(file)) {
+      return;
+    }
+    if (fs.statSync(fullPath).isDirectory()) {
+      const relativePath = path.relative(rootDir, fullPath);
+      results.push(removeDuplicateSegments(path.join(relativePath)));
+      results = results.concat(traverseDirectory(fullPath, ignoreList));
+    }
+  });
+  return results;
+};
+
 // Start by making sure that the ".vscode" folder and the "settings.json" file exists.
-ensureVsCodeFolderAndSettingsFile()
+ensureVsCodeFolderAndSettingsFile();
 
 // Define a list of folders to ignore
 const ignoreList = ['.git', '.github', 'node_modules'];
@@ -65,12 +65,12 @@ const settingsContent = fs.readFileSync(settingsPath, 'utf8');
 const settingsObject = JSON.parse(settingsContent);
 
 // Add the new key to the settings object
-settingsObject["conventionalCommits.scopes"] = traverseDirectory(rootDir, ignoreList);
-settingsObject["conventionalCommits.emojiFormat"] = "emoji"
+settingsObject['conventionalCommits.scopes'] = traverseDirectory(rootDir, ignoreList);
+settingsObject['conventionalCommits.emojiFormat'] = 'emoji';
 
 try {
-    // Write the updated settings object to the settings.json file
-    fs.writeFileSync(settingsPath, JSON.stringify(settingsObject, null, 2));
+  // Write the updated settings object to the settings.json file
+  fs.writeFileSync(settingsPath, JSON.stringify(settingsObject, null, 2));
 } catch (error) {
-    console.log("Something failed..", error)
+  throw ('Something failed..', error);
 }
